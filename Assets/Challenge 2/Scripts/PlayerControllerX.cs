b92c36ef756e7fe8f47f398c51eeee060d219868
Glenn R. Golden <ggolden22@mac.com>
2022-10-19 15:43:28 -0400

    added challenge

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float delayAfterFire = 2.0f;
+
+    public float fireAgainAfter = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float now = Time.fixedTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && (now > fireAgainAfter))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            fireAgainAfter = now + delayAfterFire;
+        }
+    }
+}
---
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 4.0f;
+
+    private int tick = 0;
+    private int tickTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tickTarget = Random.Range(1, 5);
+        InvokeRepeating("SpawnRandomBall", startDelay, 1);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall()
+    {
+        tick++;
+
+        if (tick > tickTarget)
+        {
+            tick = 0;
+            tickTarget = Random.Range(2, 8);
+
+            // Generate random ball index and random spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+            // instantiate ball at random spawn location
+            Instantiate(ballPrefabs[Random.Range(0, 3)], spawnPos, ballPrefabs[0].transform.rotation);
+        }
+    }
+
+}
